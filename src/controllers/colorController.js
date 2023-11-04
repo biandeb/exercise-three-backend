400092fd2936679207367754adf0ea78fbf2aa97
Bianca Deblinger <biandeb96@gmail.com>
2023-11-03 21:14:27 -0300

    termino con POST color

--- src/controllers/colorController.js
+++ src/controllers/colorController.js
@@ -13,3 +13,32 @@ export const getColors = async (_, res) => {
     });
   }
 };
+
+export const postColor = async (req, res) => {
+  const { body } = req;
+
+  const newColor = new ColorModel({
+    name: body.name,
+    hexa: body.hexa,
+    rgb: body.rgb,
+  });
+
+  try {
+    await newColor.save();
+
+    res.status(201).json({
+      message: 'Color creado exitosamente 🌈',
+    });
+  } catch (e) {
+    if (e.message.includes('duplicate')) {
+      res.status(400).json({
+        message: 'El color ya está en uso.',
+      });
+      return;
+    }
+    res.status(500).json({
+      message: 'Ocurrió un error guardando el color.',
+      error: e.message,
+    });
+  }
+};
--- src/routes/colorRoutes.js
+++ src/routes/colorRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 
-import { getColors } from '../controllers/colorController.js';
+import { getColors, postColor } from '../controllers/colorController.js';
 
 const router = express.Router();
 
 router.get('/', getColors);
+
+router.post('/', postColor);
 
 export default router;
