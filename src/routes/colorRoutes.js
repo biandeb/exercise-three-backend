73e4cdd3f74f14e9342405c2e646905af6908d4a
Bianca Deblinger <biandeb96@gmail.com>
2023-11-08 20:37:58 -0300

    termino ejercicio 2

--- src/routes/colorRoutes.js
+++ src/routes/colorRoutes.js
@@ -1,14 +1,11 @@
 import express from 'express';
 
 import { getColors, postColor } from '../controllers/colorController.js';
-import { getUsers, postUser } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.get('/', getColors);
-router.get('/', getUsers);
 
 router.post('/', postColor);
-router.post('/', postUser);
 
 export default router;
